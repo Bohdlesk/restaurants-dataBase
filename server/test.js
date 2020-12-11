d341ba93ab53e3919c1d7b32ddd4ab3c86f71cb3
Bohdlesk <bohdanleskovets@gmil.com>
2020-12-11 13:52:28 +0200

    1

--- server/test.js
+++ server/test.js
@@ -1,6 +1,8 @@
 
 //testing line
 
+//test
+
 const students = [
     { id: 1, firstName: 'Василий', lastName: 'Иванов', age: 14, weight: 45, height: 160, math: 4, chemistry: 5, physics: 3 },
     { id: 2, firstName: 'Андрей', lastName: 'Петров', age: 15, weight: 55, height: 163, math: 3, chemistry: 3, physics: 3 },
