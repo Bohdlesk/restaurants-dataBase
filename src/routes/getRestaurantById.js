a9208bb38faf700386da50b60c358be3e4de42de
Bohdlesk <bohdanleskovets@gmail.com>
2021-01-31 14:52:40 +0200

    added callback function

--- src/routes/getRestaurantById.js
+++ src/routes/getRestaurantById.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const {getRestaurantById} = require("../db");
 
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
     try {
-
+        const restaurantId = req.params.id;
+        const queryResult = await getRestaurantById(restaurantId)
+        res.status(200).json({
+            status: 'success',
+            restaurant: queryResult.rows[0]
+        });
     } catch (error) {
         res.status(404).json({
             status: 'error',
