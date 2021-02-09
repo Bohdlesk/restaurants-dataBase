c018839ae42af5b4f4c102f64099a2179870dfd9
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-09 12:18:43 +0200

    restaurant create router update. added error handling

--- src/dataBase/restaurantsQueryes.js
+++ src/dataBase/restaurantsQueryes.js
@@ -10,6 +10,13 @@ async function getRestaurantsList(paramOfSort, typeOfSort) {
     }
 }
 
+async function createRestaurant(request) {
+    return await client
+        .query('INSERT INTO "public"."restaurants" (name, location, price_range, website)' +
+            ' values ($1, $2, $3, $4) returning *',
+            [request.body.name, request.body.location, request.body.price_range, request.body.website])
+}
+
 function getRestaurantById(restaurantId) {
     return client
         .query('SELECT * FROM "public"."restaurants" where id = $1', [restaurantId]);
@@ -19,3 +26,14 @@ function deleteRestaurant(restaurantId) {
     return client
         .query('DELETE FROM restaurants WHERE id = $1', [restaurantId])
 }
+
+async function searchRestaurantByNameAndLocation(restaurantData){
+    const isFound = await client
+        .query('SELECT * FROM restaurants WHERE location = $1 AND name = $2',
+            [restaurantData.location, restaurantData.name])
+    return isFound
+}
+
+
+module.exports = {getRestaurantsList, getRestaurantById, deleteRestaurant, createRestaurant,
+    searchRestaurantByNameAndLocation}
