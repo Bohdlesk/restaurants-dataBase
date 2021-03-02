652e92dca9e8cb032ad5861fd0023cc0d6f68ac8
Bohdlesk <bohdanleskovets@gmail.com>
2021-03-02 18:31:59 +0200

    application rebuild

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+
+import { connectToDataBase } from './database';
+import { apiV1Router } from './routes';
+
+export const app = express();
+
+// CORS options
+const corsOptions = {
+  origin: 'https://restaurantpetproject.netlify.app/',
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log('test');
+  res.send('hi');
+});
+
+app.use('/api/v1/', apiV1Router);
+
+// connection to data base
+connectToDataBase();
