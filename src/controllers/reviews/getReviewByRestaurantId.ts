391360f619c81a350e285fcfb2969be7a99b1195
Bohdlesk <bohdanleskovets@gmail.com>
2021-03-04 21:09:02 +0200

    changed response if reviews list is empty

--- src/controllers/reviews/getReviewByRestaurantId.ts
+++ src/controllers/reviews/getReviewByRestaurantId.ts
@@ -4,17 +4,10 @@ import Review from '../../models/review';
 export default async (req: Request, res: Response): Promise<void> => {
   try {
     const review = await Review.findOne({ where: { rest_id: req.params.id } });
-    if (review) {
-      res.status(200).json({
-        status: 'success',
-        review,
-      });
-    } else {
-      res.status(404).json({
-        status: 'error',
-        message: 'Not found',
-      });
-    }
+    res.status(200).json({
+      status: 'success',
+      review,
+    });
   } catch (error) {
     res.status(500).json({
       message: error.message,
