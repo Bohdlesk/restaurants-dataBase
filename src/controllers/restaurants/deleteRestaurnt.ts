5d231146358898dbb033aebb3a931cd80681394c
Bohdlesk <bohdanleskovets@gmail.com>
2021-03-04 21:59:01 +0200

    delete restaurant key reference error fix

--- src/controllers/restaurants/deleteRestaurnt.ts
+++ src/controllers/restaurants/deleteRestaurnt.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { Restaurant } from '../../models';
+import Review from '../../models/review';
 
 export default async (req: Request, res: Response): Promise<void> => {
   try {
     const { id } = req.params;
+    await Review.destroy({ where: { rest_id: id } });
     const isDeleted = await Restaurant.destroy({ where: { id } });
     if (isDeleted) {
       res.status(200).json({
