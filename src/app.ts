2362bea7726b2d605982bd484cc9e3883b1627b1
Bohdlesk <bohdanleskovets@gmail.com>
2021-08-01 12:56:34 +0300

    Cors fixed

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/', (req: express.Request, res: express.Response) => {
   console.log('test');
